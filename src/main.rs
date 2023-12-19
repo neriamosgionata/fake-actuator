bd991a020a07b2960bc2e3581cc14b4a33feae11
Amos Gionata Neri <a.neri@prowebconsulting.net>
2023-12-19 12:33:25 +0100

    changed returns of server, added pulse

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+use std::fs;
 use std::net::SocketAddr;
+use std::thread::spawn;
 use coap_lite::{CoapRequest, RequestType};
 use serde_json::json;
 use local_ip_address::local_ip;
 use fake_actuator::{CoAPClient, Server};
 use anyhow::Result;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct RegisterResponse {
+    id: i32,
+    state: bool,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -21,9 +30,17 @@ async fn main() -> Result<()> {
 
     println!("Local IP address: {}", actuator_ip_address);
 
+    let is_pulse = match fs::read_to_string(".is_pulse") {
+        Ok(_) => true,
+        Err(_) => false
+    };
+
     let register_params = json! {
         {
             "ip_address": actuator_ip_address,
+            "online": true,
+            "state": false,
+            "pulse": is_pulse,
             "port": actuator_port,
         }
     }.to_string().as_bytes().to_vec();
@@ -36,17 +53,21 @@ async fn main() -> Result<()> {
         return Ok(());
     }
 
-    let _actuator_id = new_actuator.parse::<i32>().unwrap();
+    let register_response: RegisterResponse = serde_json::from_str(new_actuator.as_str()).expect("Unable to parse JSON");
 
-    run_server(actuator_port).await;
+    fs::write(".status", if register_response.state { "ON" } else { "OFF" }).expect("Unable to write file");
+
+    println!("Actuator state: {:?}", if register_response.state { "ON" } else { "OFF" });
+
+    run_server(actuator_ip_address, actuator_port).await;
 
     Ok(())
 }
 
-async fn run_server(actuator_port: i16) {
-    println!("Running server");
+async fn run_server(actuator_ip_address: String, actuator_port: i16) {
+    let address = actuator_ip_address + ":" + actuator_port.to_string().as_str();
 
-    let address = "127.0.0.1:".to_owned() + actuator_port.to_string().as_str();
+    println!("Running server on {}", address);
 
     let mut server = Server::new(address).unwrap();
 
@@ -55,6 +76,8 @@ async fn run_server(actuator_port: i16) {
             let request_ref = &request;
 
             let payload = callback(request_ref).await;
+
+            println!("New state: {}", payload);
 
             match request.response {
                 Some(mut message) => {
@@ -73,20 +96,44 @@ async fn run_server(actuator_port: i16) {
 async fn callback(request: &CoapRequest<SocketAddr>) -> String {
     println!("Callback called");
 
-    let payload = String::from_utf8(request.message.payload.clone()).unwrap();
-
-    if request.get_method() != &RequestType::Post {
-        println!("Not a POST request");
+    if request.get_method() != &RequestType::Post && request.get_method() != &RequestType::Get {
+        println!("Unknown method");
         return "KO".to_string();
     }
+
+    if request.get_method() == &RequestType::Get {
+        return fs::read_to_string(".status").unwrap_or_else(|_| "KO".to_string());
+    }
+
+    let payload = String::from_utf8(request.message.payload.clone()).unwrap();
 
     println!("POST request");
 
     if payload == "ON" {
-        println!("ON");
+        match fs::write(".status", "ON") {
+            Ok(_) => {}
+            Err(_) => {}
+        };
         "ON".to_string()
+    } else if payload == "ON-PULSE" {
+        spawn(|| {
+            std::thread::sleep(std::time::Duration::from_millis(750));
+
+            match fs::write(".status", "OFF") {
+                Ok(_) => {}
+                Err(_) => {}
+            };
+        });
+        match fs::write(".status", "ON-PULSE") {
+            Ok(_) => {}
+            Err(_) => {}
+        };
+        "ON-PULSE".to_string()
     } else if payload == "OFF" {
-        println!("OFF");
+        match fs::write(".status", "OFF") {
+            Ok(_) => {}
+            Err(_) => {}
+        };
         "OFF".to_string()
     } else {
         println!("Unknown command");
